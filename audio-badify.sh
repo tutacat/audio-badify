fd29117388cf771db38709eb903ad2b00466740f
tutacat <fhwbml@gmail.com>
2020-06-18 20:55:11 +0100

    fix case and improve *

--- audio-badify.sh
+++ audio-badify.sh
@@ -1,16 +1,16 @@
-EXEC="ffmpeg"
-YYY=""
+EXEC='ffmpeg'
+YYY=''
 MODE=1
-
+echo "$@"
 POSITIONAL=()
+
 while [[ $# -gt 0 ]]
 do
-key="$1"
-
-case $key in
+  key="$1"
+  case $key in
     -h|--help)
-    echo -e "Audio Badify script (ffmpeg) usage.\n\naudio-badify -[m] <input> <output>-h\t--help\tShow this help.\n-m\t--mode\tBadify mode, currently 1 (22.5Khz), or 2 (8Khz)\n\t--exec\tSpecify the ffmpeg executable\n-y\t\tffmpeg assume yes (overwrite, etc.)"
-    shift
+    echo -e "Audio Badify script (ffmpeg) usage.\n\naudio-badify -[m] <input> <output>\n-h  --help  Show this help.\n-m  --mode  Badify mode, currently 1 (22.5Khz), or 2 (8Khz)\n    --exec  Specify the ffmpeg executable\n-y          ffmpeg assume yes (overwrite, etc.)"
+    exit 1
     shift
     ;;
     -m|--mode)
@@ -26,14 +26,28 @@ case $key in
     -y)
     YYY="-y"
     shift
+    ;;
+    *)
+    POSITION+=("$1")
     shift
     ;;
-esac
+  esac
 done
-set -- "${POSITIONAL[@]}"
 
-if [ ${MODE} -eq 1 ] then
+set -- "${POSITIONAL[@]}"
+echo check args
+echo "$@"
+if [ ! -z $1 ] && [ ! -z $2 ]; then
+  echo yes
+  if [ ${MODE} -eq 1 ]; then
+    echo mode 1
     ${EXEC} -i "$1" -ar 22050 -b:a 1k ${YYY} "$2"
-elif [ ${MODE} -eq 2] then
+  elif [ ${MODE} -eq 2 ]; then
+    echo mode 2
     ${EXEC} -i "$1" -ar 8000 -b:a 100 ${YYY} "$2"
+  else
+    echo "Incorrect mode specified"
+  fi
 fi
+
+exit 1
